c8124b77eee418edaed9bc47b32bcb9bc27f95aa
benjamin mayeur <62392557+benHuey1@users.noreply.github.com>
2023-05-10 19:25:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,21 +15,34 @@ console.log(tab);
 /* Program */
 let previousDivMole;
 let upMole;
-let score = 0;
+let score = 1;
 function divInGray() {
     if (previousDivMole) {
-        previousDivMole.style.backgroundColor = "white";
+        previousDivMole.style.backgroundColor = "rgb(125, 75, 10)";
+        previousDivMole.style.boxShadow =
+            "10px 10px 5px 0px rgba(0,0,0,0.5) inset";
+        previousDivMole.style.backgroundImage = "none";
+        previousDivMole.style.border = "2px solid black";
     }
     let randNum = Math.floor(Math.random() * tab.length);
     let divMole = document.getElementById(`${randNum}`);
-    divMole.style.backgroundColor = "gray";
+    // divMole.style.backgroundColor = "gray";
+    divMole.style.backgroundImage =
+        "url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngmart.com%2Ffiles%2F13%2FExpression-Cartoon-Eyes-PNG-Background-Image.png&f=1&nofb=1&ipt=fd9ea1ab271147a517b6f6c89472205279c558bfd3f91d0902323d30aaffaed2&ipo=images)";
+    divMole.style.backgroundRepeat = "no-repeat";
+    divMole.style.backgroundSize = "contain";
+    divMole.style.background = "scale(0.5)";
+    divMole.style.backgroundColor = "rgb(95, 95, 95)";
+    divMole.style.backgroundPosition = "center";
+    divMole.style.boxShadow = "10px 10px 5px 0px rgba(0,0,0,0.5)";
+    divMole.style.border = "2px solid grey";
     console.log(divMole);
     previousDivMole = divMole;
     upMole = randNum;
     function clickMole() {
         if (upMole === randNum) {
-            score++;
             divScore.textContent = score;
+            score++;
         } else console.log("sorry");
     }
     divMole.addEventListener("click", clickMole, { once: true });
